c1e09ddd2b3b8d5ac3cb1a8bbb5c0b4eeecc11d4
Jon Allars <jon@allars.co.uk>
2014-01-30 15:37:50 +0000

    Fixed issue with showing members forum account in the list. This fix will no doubt need applying elsewhere but this will be done in the V2 mod.

--- Sources/ycc/YCCMemberList.php
+++ Sources/ycc/YCCMemberList.php
@@ -10,33 +10,27 @@ function YCCMemberList() {
 
     isAllowedTo(array('committee'));
     $manager = allowedTo('committee_manage');
-    
+
     $memberDB = array("localhost", "yorkcano_web", "web", "yorkcano_smf", true);
 
     $memberConnection = mysql_connect($memberDB[0], $memberDB[1], $memberDB[2], true) or die("Could not connect: " . mysql_error());
     mysql_select_db($memberDB[3], $memberConnection) or die ('Cannot Connect to DB: ' . mysql_error());
 
-    $memberQuery = "select 
+    $memberQuery = "select
                     members.*,
-                    fNames.forumNames,
-                    DATE_FORMAT(members.membershipStarted, '%d/%m/%Y') as membershipStartedF, 
-                    DATE_FORMAT(members.membershipExpires, '%d/%m/%Y') as membershipExpiresF 
-                   from ycc_members as members 
-                   left join (
-                     select xref.membershipNumber, Group_Concat(forum.real_name) as forumNames
-                     from ycc_memberForumCR as xref
-                     left join smf_members as forum
-                     on xref.forumId = forum.id_member
-                     group by xref.membershipNumber
-                   ) as fNames
-                   on members.membershipNumber = fNames.membershipNumber
+                    forum.*,
+                    DATE_FORMAT(members.membershipStarted, '%d/%m/%Y') as membershipStartedF,
+                    DATE_FORMAT(members.membershipExpires, '%d/%m/%Y') as membershipExpiresF
+                   from ycc_members as members
+                   left join smf_members as forum
+                   on members.forumId = forum.id_member
                    order by members.membershipNumber";
-    
+
     $result = mysql_query($memberQuery, $memberConnection);
 
     $memberCount = mysql_num_rows($result);
     $members = array();
-    
+
     while ($row = mysql_fetch_assoc($result)) {
 
         $contactsQuery = "select * from ycc_memberscontacts where membershipNumber = " . $row["membershipNumber"] . " and contactsSequence = 10";
@@ -54,16 +48,16 @@ function YCCMemberList() {
          || $row["memberContact"] == ""
          || $contactsRow["contactsName"] == ""
          || $contactsRow["contactsRelationship"] == ""
-         || $contactsRow["contactsPhone1"] == ""       
+         || $contactsRow["contactsPhone1"] == ""
         ) {
             $row["complete"] = "Details Missing";
         } else {
             $row["complete"] = "&nbsp;";
         }
-        
+
         array_push($members, $row);
 
-    }        
+    }
 
     $context['pageTitle'] = 'YCC Members';
     $context['memberCount'] = $memberCount;
--- Themes/default/ycc/YCCMemberList.template.php
+++ Themes/default/ycc/YCCMemberList.template.php
@@ -1,13 +1,13 @@
 <?php
 
 function template_main() {
-    
+
     global $context, $settings, $options, $txt, $scripturl;
-    
+
     $members = $context["memberArray"];
     $manager = $context["memberManage"];
     $seasons = $context["seasons"];
-    
+
     ?>
 
 <table id="memberList" class="membershipTable">
@@ -46,17 +46,17 @@ function template_main() {
         </tr>
     </tfoot>
     <tbody>
-    
+
         <?php
-        
+
         foreach ($members as $member) {
-    
+
             ?>
-        
+
             <tr id="<?php print($member["membershipNumber"]); ?>" class="clickableRow">
                 <td><?php print($member["membershipNumber"] . "/" . $member["membershipType"]); ?></td>
                 <td><?php print($member["memberFirstName"] . " " . $member["memberLastName"]); ?></td>
-                <td><?php print($member["forumNames"]); ?></td>
+                <td><?php print($member["member_name"]); if ($member["member_name"] != $member["real_name"]) { print(" (" . $member["real_name"] . ")"); } ?></td>
                 <td><?php print($member["membershipExpiresF"]); ?></td>
                 <td><?php print($member["memberBcuNumber"]); ?></td>
                 <td><?php print(getStatusDesc($member["membershipStatus"])); ?></td>
@@ -64,24 +64,24 @@ function template_main() {
                 <td><?php if ($manager) { ?><a href="index.php?action=ymembershipdetail&yno=<?php print($member["membershipNumber"]); ?>&from=ylist">Edit</a><?php } ?></td>
             </tr>
 
-            <?php 
-     
-            
+            <?php
+
+
         }
-     
+
         ?>
-    
+
     </tbody>
 </table>
 
 <script type="text/javascript">
-    
+
     var pagesize = 15;
-    
+
     $(document).ready(function() {
-    
+
         $("#memberList tbody").click(function(event) {
-      
+
             var target = event.target;
             var jTarget = $(target);
             var className = target.className;
@@ -99,43 +99,43 @@ function template_main() {
             if (jTarget.is("input")) {
 
                 return true;
-                
+
             } else {
-             
+
                 if (!row.find("td:first").hasClass("dataTables_empty")) {
                     location.href = "index.php?action=ymembershipdetail&yno=" + key + "&from=ylist";
                 }
-                
+
             }
-            
+
             return false;
 
         });
-        
+
         var memberListTable = defineTableNew("#memberList", [], [7], [], "YCC_YCCMEMBERLIST", "No Members Found", tableLayout, true);
         memberListTable.fnRestoreAllFilters();
-        
+
         $("#search5").html(fnCreateSelect( memberListTable.fnGetColumnData(5, true, false, false)));
-	  
+
 	$("#search5 select").change(function() {
             memberListTable.fnFilter($(this).val(), 5);
         });
 
 
         $("#search6").html(fnCreateSelect( memberListTable.fnGetColumnData(6, true, false, false)));
-	  
+
 	$("#search6 select").change(function() {
             memberListTable.fnFilter($(this).val(), 6);
         });
-        
+
         memberListTable.fnRestoreAllFilters();
-        
+
     });
 
 </script>
 
-<?php 
-    
+<?php
+
 }
 
 ?>
